0b456c565b18eb799bcf71ca023f252dea9fc119
Midhun P M <midhun@websoftbay.com>
2021-12-24 01:25:23 +0530

    Authcode link generation added

--- test/HelloTest.php
+++ test/HelloTest.php
@@ -6,7 +6,11 @@ final class HelloTest extends TestCase
 {
     public function testHello()
     {
-        $client = new FyersClient();
-        $this->assertSame("1.0.0", $client->getVersion());
+        $client = new FyersClient('fsdf', 'asdasd');
+        $generator = $client->getCodeGenerator();
+        $generator->addHandler("https://trade.fyers.in/api-login/redirect-uri/index.html");
+        $generator->addState("fdsf");
+        
+        print_r($generator->getCodeGenerationLink());
     }
 }
